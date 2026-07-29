9d3af239fc67f089c46faedfab463a112fb6bcdd
yabumac <yabumac01@gmail.com>
2026-07-29 23:54:09 +0300

    update after vercel

--- api/index.py
+++ api/index.py
@@ -35,14 +35,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # Register handlers
 ptb_app.add_handler(CommandHandler("start", start))
 
-@app.post("/")
+@app.post("/api/index.py")
 @app.post("/api/webhook")
+@app.post("/")
 async def webhook_handler(request: Request):
     """Handles incoming updates from Telegram"""
     data = await request.json()
     update = Update.de_json(data, ptb_app.bot)
 
-    # Run the update through the PTB application logic
     async with ptb_app:
         await ptb_app.process_update(update)
 
